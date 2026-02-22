76c4fc2cd169e547fa33048ee952c3831ce220e3
Sophanarath <www.sophanarath77@gmail.com>
2026-02-22 21:58:04 +0700

    initialize za-warudo

---
+++ android/app/src/main/kotlin/com/example/za_warudo_time_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.za_warudo_time_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/loading.dart';
+import 'pages/location.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
